8824011bf146f2ba8e627d1b3c1fdec107f1f70d
Skorpiyo <skorpio277@gmail,com>
2022-09-17 17:26:28 +0300

    SettingTypes: appropriated type FormulaCreator: use String

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char C = 'C';
+        C = 97;
+        char H = 'H';
+        H = 104;
+        char A = 'A';
+        A = 99;
+        char R = 'R';
+        R = 114;
+        System.out.println(C);
+        System.out.println(H);
+        System.out.println(A);
+        System.out.println(R);
+
     }
 }
